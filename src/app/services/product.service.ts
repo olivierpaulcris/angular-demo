3d9753440215ed2fa628a1e1e7b30d25146bd2d3
Paul Cristerna <olivierpaulcristerna@gmail.com>
2022-09-06 18:01:33 -0600

    Nuevos cambios

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, catchError, tap } from "rxjs";
 import { IProduct } from "../models/product";
 
 @Injectable({
@@ -8,38 +8,43 @@ import { IProduct } from "../models/product";
 })
 export class ProductService {
 
-    private productUrl = 'www.myWebService.com/api/products';
+    private productUrl = 'api/products/products.json';
 
     constructor(
         private http: HttpClient
     ) { }
 
-    getProducts(): IProduct[] {
-        return [
-            {
-                "productId": 2,
-                "productName": "Combo  Marsh Mallows",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2021",
-                "description": "Acme Products",
-                "price": 32.99,
-                "starRating": 4.2,
-                "imageUrl": "https://m.media-amazon.com/images/I/41rBSsxxIwL.jpg"
-            },
-            {
-                "productId": 3,
-                "productName": "Strawberry Marsh Mallows",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2021",
-                "description": "Acme Products",
-                "price": 50.23,
-                "starRating": 2.1,
-                "imageUrl": "https://5.imimg.com/data5/SELLER/Default/2020/11/NU/WU/DA/8735450/81qqdq8ksgl-sl1500--250x250.jpg"
-            },
-        ];
+    // getProducts(): IProduct[] {
+    //     return [
+    //         {
+    //             "productId": 2,
+    //             "productName": "Combo  Marsh Mallows",
+    //             "productCode": "GDN-0023",
+    //             "releaseDate": "March 18, 2021",
+    //             "description": "Acme Products",
+    //             "price": 32.99,
+    //             "starRating": 4.2,
+    //             "imageUrl": "https://m.media-amazon.com/images/I/41rBSsxxIwL.jpg"
+    //         },
+    //         {
+    //             "productId": 3,
+    //             "productName": "Strawberry Marsh Mallows",
+    //             "productCode": "GDN-0023",
+    //             "releaseDate": "March 18, 2021",
+    //             "description": "Acme Products",
+    //             "price": 50.23,
+    //             "starRating": 2.1,
+    //             "imageUrl": "https://5.imimg.com/data5/SELLER/Default/2020/11/NU/WU/DA/8735450/81qqdq8ksgl-sl1500--250x250.jpg"
+    //         },
+    //     ];
+    // }
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl).pipe(tap(data => console.log('All: ', JSON.stringify(data))),
+            catchError(this.handleError))
     }
 
-    // getProducts(): Observable<IProduct[]> {
-    //     return this.http.get<IProduct[]>(this.productUrl);
-    // }
+    private handleError(err: HttpErrorResponse) {
+
+    }
 }
